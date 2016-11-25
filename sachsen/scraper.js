240eef18db71e11661d895cf24ba5e0907fb62e3
Knut Perseke <knut.perseke@gmail.com>
2016-11-25 09:50:44 +0100

    udate sachsen scrape and write json files for each school

--- sachsen/scraper.js
+++ sachsen/scraper.js
@@ -1,5 +1,8 @@
 var scrapyard = require("scrapyard");
 var $ = require('cheerio');
+var fs = require('fs');
+
+var file = 'data.json';
 
 var scraper = new scrapyard({
     debug: true,
@@ -45,7 +48,7 @@ function scrape(url, method = 'GET', formdata) {
                 if (responseIsValid($))
                     fulfill($);
                 else {
-                    reject(new Error('Error detected'));
+                    reject(err);
                 }
             }
         });
@@ -87,8 +90,12 @@ function scrapeAllSchools(schools) {
     return schools.reduce(function(acc,curr) {
         return acc.then(function(res) {
             return scrapeSchool(curr).then(function(result) {
-                res.push(result);
-                return res;
+                //res.push(result);
+                //return res;
+                console.log('finished');
+            }).catch(function(err) {
+                console.log('Error cathed');
+                //return res;
             });
         });
     }, Promise.resolve([]));
@@ -101,19 +108,25 @@ function setActiveSchool(schooldata) {
 
 function scrapeSchool(schooldata) {
     //Set Active School
-    return scrapePotrait(schooldata)
+    console.log('scraping ' + schooldata.bez);
+    return scrapePortrait(schooldata)
     .then(function(data) {
-        var scrapeArray = [];
-        scrapeArray.push(scrapeStudents());
+        var scrapeArray = [data, scrapeStudents(), scrapeStaffResources(), scrapeFacturalResources(), scrapeTeachAndLearn(),
+            scrapeSchoolRoutine(), scrapeIndividualFacilitation(), scrapeProfessionality(), scrapeQuality(), scrapeCooperation(),
+            scrapeEducationSuccess()
+        ];
         return Promise.all(scrapeArray);
     })
     .then(function(data) {
-        // TODO: Merge Data into reasonable Data Format
-        console.log(data)
-    });
+
+        fs.writeFile(__dirname + '/output/' + schooldata.bez.replace(/[\\/\\.]/g, '').replace(/\s/g,"_") + '.json', JSON.stringify(data), {encoding: 'utf-8',flag: 'w+'}, function (err) {
+            console.error(err)
+
+        })
+    })
 }
 
-function scrapePotrait(schooldata) {
+function scrapePortrait(schooldata) {
     return scrape('https://schuldatenbank.sachsen.de/index.php?id='+ informationDictionary.potrait, 'POST', schooldata)
         .then(function($) {
             var portrait = $('.kontaktliste').find('li').get().reduce((prev, entry, index) => {
@@ -128,6 +141,30 @@ function scrapePotrait(schooldata) {
         });
 }
 
+function getSections($) {
+
+    var sections = $('#content h2').length > 0 ? $('#content h2') : $('#content h3')
+
+    return $(sections).get().map((section) => {
+        var key = $(section).text();
+        var entries = $(section).nextUntil('h2').get();
+        var sourcetext = undefined;
+        var lastEntry = entries.pop();
+        if ($(lastEntry).hasClass('small')) {
+            sourcetext = $(lastEntry).text();
+        } else {
+            entries.push(lastEntry);
+        }
+
+        var result = {description: key, entries:entries};
+        if (sourcetext) {
+            result.source = sourcetext
+        }
+        return result
+    });
+
+}
+
 function scrapeStudents() {
     var url = 'https://schuldatenbank.sachsen.de/index.php?id=' + informationDictionary.students;
     return scrape(url)
@@ -136,13 +173,9 @@ function scrapeStudents() {
             var requests = years.map((year) => {
                 return scrape(url, "POST", {jahr: year})//, 'POST', {jahr : year})
                     .then(function($) {
-                        var sections = $('#content h2').get().map((section) => {
-                            var key = $(section).text();
-                            var value = $(section).next('table').get();
-                            return {description: key, table:value};
-                        });
+                        var sections = getSections($);
                         sections = sections.map((section) => {
-                            section.table = parseTable(section.table);
+                            section.entries = parseTable(section.entries);
                             return section;
                         });
                         return sections;
@@ -154,10 +187,101 @@ function scrapeStudents() {
                     acc[years[index]] = table;
                     return acc;
                 }, {});
-            }).then(function(data) {
-                console.log(data);
-            });
+            })
         });
+}
+
+function scrapeStaffResources() {
+    var url = base_url + '?id=' + informationDictionary.staff_ressources;
+    return scrape(url)
+        .then(getSections)
+        .then(parseSections)
+}
+
+function scrapeFacturalResources() {
+    var url = base_url + '?id=' + informationDictionary.factual_ressources;
+    return scrape(url)
+        .then(getSections)
+        .then(parseSections)
+}
+
+function scrapeTeachAndLearn() {
+    var url = base_url + '?id=' + informationDictionary.learning;
+    return scrape(url)
+        .then(getSections)
+        .then(parseSections)
+}
+
+function scrapeSchoolRoutine() {
+    var url = base_url + '?id=' + informationDictionary.events;
+    return scrape(url)
+        .then(getSections)
+        .then(parseSections)
+}
+
+function scrapeIndividualFacilitation() {
+    var url = base_url + '?id=' + informationDictionary.individual_facilitation;
+    return scrape(url)
+        .then(getSections)
+        .then(parseSections)
+}
+
+function scrapeProfessionality() {
+    var url = base_url + '?id=' + informationDictionary.working_groups;
+    return scrape(url)
+        .then(getSections)
+        .then(parseSections)
+}
+
+function scrapeQuality() {
+    var url = base_url + '?id=' + informationDictionary.quality;
+    return scrape(url)
+        .then(getSections)
+        .then(parseSections)
+}
+
+function scrapeCooperation() {
+    var url = base_url + '?id=' + informationDictionary.cooperation;
+    return scrape(url)
+        .then(getSections)
+        .then(parseSections)
+}
+
+function scrapeEducationSuccess() {
+    var url = base_url + '?id=' + informationDictionary.success;
+    return scrape(url)
+        .then(getSections)
+        .then(parseSections)
+}
+
+function parseSections(sections) {
+    return sections.map((section) => {
+        section.parsedData = section.entries.map((entry) => {
+            if (entry) {
+                entry = parseSectionEntry(entry);
+                return entry
+            }
+        });
+        delete section.entries;
+        return section;
+    })
+}
+
+function parseSectionEntry(entry) {
+    switch(entry.name) {
+        case 'table':
+            return parseTable(entry);
+            break;
+        case 'p' :
+            return $(entry).text();
+            break;
+        case 'ul' :
+            return parseList(entry);
+            break;
+        default:
+            //console.log('Unhandled Section Type "' + entry.name + '" detected');
+            break;
+    }
 }
 
 function parseTable(table) {
@@ -166,7 +290,37 @@ function parseTable(table) {
     var keys = $(rows[0]).find('td').get().map((cell) => $(cell).text().replace(/\\n/g, '').trim().replace(/\s/g, '_'));
     var valueRows = rows.slice(1);
     var values = valueRows.map((row) => {
-        return $(row).find('td').get().map((cell) => $(cell).text());
+        return $(row).find('td').get().map((cell) => {
+            // Case 1: text
+            var text = $(cell).text().trim();
+            if (text) {
+                return text
+            }
+            //Case 2: Image
+            var img = $(cell).find('img')
+            if (img) {
+                if ($(img).attr('alt')) {
+                    return $(img).attr('alt')
+                } else {
+                    return $(img).attr('src')
+                }
+            }
+            //Case 3: button
+            // var form = $(cell).find('form');
+            // if (form) {
+            //     var fields = $(form).find('input');
+            //     var url = $(form).attr('action').split('?')[1];
+            //     var formdata = fields.reduce((prev, field) => {
+            //         prev[$(field).attr('name')] = $(field).attr('value');
+            //     }, {});
+            //     var subdata = scrape(base_url + url, 'POST', formdata)
+            //         .then(function(data) {
+            //             console.log(data);
+            //         })
+            // }
+
+
+        });
     });
 
     var table = values.map((row) => {
@@ -175,19 +329,20 @@ function parseTable(table) {
             return acc;
         }, {})
     })
-    console.log(table);
+    //console.log(table);
     return table;
 }
 
-function scrapeMultiValuePage(scrape) {
-
+function parseList(list) {
+    var entries = $(list).find('li').get();
+    var parsed = entries.map(function(el) {
+        return $(el).text();
+    });
+    return parsed;
 }
 
 getMainpage()
     .then(getSchoolpage)
     .then(getScrapeDataForSchools)
     .then(scrapeAllSchools)
-    .then(function(data) {
-        console.log(data);
-    })
     .catch(console.log);
